d2f9f3fde472c0d4cb914d8498dfef378f711c58
Lilia Simeonova <lilia.valentinova.simeonova@gmail.com>
2021-03-27 01:38:01 +0200

    first commit

---
+++ packages/themes/src/MobileApp/assets/Mobile App _ Pagerland Next_files/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/mobile-app":["static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js","static\u002Fchunks\u002Fpages\u002Fmobile-app.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fmobile-app"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
